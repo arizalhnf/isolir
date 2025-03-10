4ea1580beae53ddf87c87fb29dc637935530d606
Arizal Hanif <60518986+arizalhnf@users.noreply.github.com>
2025-03-11 04:37:17 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+
+    <title>Internet Isolir | PT. MEGA ARTHA LINTAS DATA</title>
+    
+  </head>
+  <body>
+
+    <div class="container my-3">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-8 col-lg-6 text-center">
+                <div class="pb-4">
+                    <img class="img-fluid" width="170px" src="public/img/nowifi.jpg" alt="">
+                    <h1 class="text-danger p-0 m-0"><strong>MOHON MAAF</strong></h1>
+                    <h3 class="m-0 p-0 text-primary">Internet Di Isolir Oleh Billing Sistem</h3>
+                </div>
+                <div class="border border-primary rounded p-3" style="background-color: #cfe2f5">
+                    <p>Pelanggan Internet yang terhormat</p>
+                    <p>Kami informasikan bahwa layanan internet anda saat ini sedang di <strong>Isolir</strong> secara otomatis oleh sistem billing kami.</p>
+                    <p>Dimohon untuk <strong>melakukan pembayaran tagihan</strong>, supaya internet <strong>kembali normal</strong></p>
+                    <p>Guna menghindari ketidaknyamanan ini, dimohon untuk melakukan pembayaran tepat waktu di tiap bulan nya</p>
+                    <p> Silahkan hubungi admin layanan kami mengenai hal ini</p>
+                    <p>~Terimakasih~</p>
+                </div>
+            </div>
+        </div>
+    </div>
+      
+  <script src="public/js/jquery-3.6.0.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+  </body>
+</html>
